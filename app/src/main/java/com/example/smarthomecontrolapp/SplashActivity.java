eaee30231223b491788b560b67b046053f6cd4da
fatimaKhan003 <f.khan.0211003@gmail.com>
2026-05-03 04:52:52 +0500

    added splash screen

---
+++ app/src/main/java/com/example/smarthomecontrolapp/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.smarthomecontrolapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+ImageView logo;
+TextView appname;
+TextView tagline;
+Animation logoAnim,textAnim;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+init();
+logo.startAnimation(logoAnim);
+appname.startAnimation(logoAnim);
+tagline.startAnimation(textAnim);
+        new Handler().postDelayed(()->
+        {
+            startActivity(new Intent(SplashActivity.this,register.class));
+            finish();
+        },3000);
+
+    }
+    private void init()
+    {
+        logo=findViewById(R.id.ivSplashLogo);
+        appname=findViewById(R.id.tvAppName);
+        tagline=findViewById(R.id.tvTagline);
+        logoAnim= AnimationUtils.loadAnimation(this, R.anim.fade_in_scale);
+        textAnim=AnimationUtils.loadAnimation(this,R.anim.slide_up);
+
+    }
+}

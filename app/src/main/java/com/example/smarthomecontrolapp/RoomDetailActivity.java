a48a4f9cabaec37ee80de6116a6d559da8d9ddcf
fatimaKhan003 <f.khan.0211003@gmail.com>
2026-05-03 15:50:30 +0500

    fixed the label for number of devices

--- app/src/main/java/com/example/smarthomecontrolapp/RoomDetailActivity.java
+++ app/src/main/java/com/example/smarthomecontrolapp/RoomDetailActivity.java
@@ -24,7 +24,7 @@ import java.util.List;
 
 public class RoomDetailActivity extends AppCompatActivity {
 
-    TextView tvRoomTitle, tvTemperature, tvTodayCost;
+    TextView tvRoomTitle, tvTemperature, tvTodayCost,tvDevicesLabel;
     RecyclerView recyclerRoomDevices;
     DeviceAdapter deviceAdapter;
     ArrayList<Device> deviceList;
@@ -48,6 +48,7 @@ public class RoomDetailActivity extends AppCompatActivity {
         tvTemperature = findViewById(R.id.tvTemperature);
         tvTodayCost = findViewById(R.id.tvTodayCost);
         recyclerRoomDevices = findViewById(R.id.recyclerRoomDevices);
+        tvDevicesLabel=findViewById(R.id.tvDevicesLabel);
 
 
 
@@ -112,6 +113,7 @@ public class RoomDetailActivity extends AppCompatActivity {
                 }
 
                 deviceAdapter.notifyDataSetChanged();
+                tvDevicesLabel.setText("Devices In This Room ("+ deviceList.size()+" )");
 
                 // Update cost display
                 double cost = totalPower * 0.12;
